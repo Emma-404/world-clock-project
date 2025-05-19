f1214ba67a03ca2cd8982286065f347aba876560
Emma-404 <emmamboronad@gmail.com>
2025-05-19 19:59:10 +0200

    Javascript
    -Added time Js

---
+++ javascript/index.js
@@ -0,0 +1,25 @@
+function updateLosAngelesTie() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los-Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+
+let parisElement = document.querySelector("#paris");
+
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Pairs");
+
+ parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
